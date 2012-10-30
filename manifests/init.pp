7e07855c3fc32016ddc0b7dfaf9205cd44ddfd39
Davi Campos <arlocm@gmail.com>
2012-10-30 11:34:10 -0200

    Adding default lang and language

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,7 @@
 class locale (
-  $locales
+  $locales  = 'en_US.UTF-8',
+  $lang     = 'en_US.UTF-8',
+  $language = 'en_US'
 ) {
 
   Exec {
@@ -8,11 +10,29 @@ class locale (
 
   exec { 'install_locales':
     command => "locale-gen ${locales}",
+    notify  => Exec['reload_locales']
+  }
+
+  if $lang {
+    exec { 'update_default_lang':
+      command => "update-locale LANG='${lang}'",
+      notify  => Exec['reload_locales'],
+      unless  => "locale | grep -e ^LANG=${lang}\$"
+    }
+  }
+
+  if $language {
+    exec { 'update_default_language':
+      command => "update-locale LANGUAGE='${language}'",
+      notify  => Exec['reload_locales'],
+      unless  => "locale | grep -e ^LANGUAGE=${language}\$"
+    }
   }
 
   exec { 'reload_locales':
-    command => "locale-gen",
-    require => Exec['install_locales']
+    command     => "dpkg-reconfigure locales",
+    refreshonly => true
   }
+
 }
 
